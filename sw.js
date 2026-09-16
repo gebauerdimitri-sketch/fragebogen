8456cad92290fc03fda393d71b6b8a1441d583ce
gebauerdimitri-sketch <gebauerdimitri@gmail.com>
2026-09-16 12:18:57 +0200

    Fragebogen-App als installierbare Web-App

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Offline-Cache. Bei Änderungen an der App VERSION erhöhen.
+const VERSION = "v1";
+const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+// Netzwerk zuerst (damit Updates ankommen), sonst Cache
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(r => {
+      if (r.ok && new URL(e.request.url).origin === location.origin) {
+        const copy = r.clone(); caches.open(VERSION).then(c => c.put(e.request, copy));
+      }
+      return r;
+    }).catch(() => caches.match(e.request, {ignoreSearch: true}).then(m => m || caches.match("./index.html")))
+  );
+});
