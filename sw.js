2b1565a671a2cbe658318d3a1c51bf943f115ac9
gebauerdimitri-sketch <gebauerdimitri@gmail.com>
2026-09-18 20:02:25 +0200

    Uebergang zwischen den Frageboegen im Klientenmodus

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline-Cache. Bei Änderungen an der App VERSION erhöhen.
-const VERSION = "v6";
+const VERSION = "v7";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(VERSION).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
