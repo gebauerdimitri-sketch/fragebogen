9f802634d1fa8653f6de94e4cd4d0606acda67e9
gebauerdimitri-sketch <gebauerdimitri@gmail.com>
2026-09-18 19:04:58 +0200

    FBM-36 Beziehungsmotive ergaenzt

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline-Cache. Bei Änderungen an der App VERSION erhöhen.
-const VERSION = "v4";
+const VERSION = "v5";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(VERSION).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
